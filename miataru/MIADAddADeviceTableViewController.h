4e4acb3fbfa86a33c7c4e5b632861c7c2293b730
Daniel Kirstenpfad <bietiekay@miataru.com>
2013-09-08 16:46:32 +0200

    added AddADeviceTableViewController and handling - corrected with static tableview cells

---
+++ miataru/MIADAddADeviceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MIADAddADeviceTableViewController.h
+//  miataru
+//
+//  Created by Daniel Kirstenpfad on 08.09.13.
+//  Copyright (c) 2013 Miataru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MIADAddADeviceTableViewController : UITableViewController
+
+@end
