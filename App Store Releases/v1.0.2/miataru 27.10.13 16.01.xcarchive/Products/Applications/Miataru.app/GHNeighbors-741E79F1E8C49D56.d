d5f228901f70360d61d8fb2defe2d0eae8e0acfc
Daniel Kirstenpfad <bietiekay@miataru.com>
2013-10-30 23:31:08 +0100

    v1.0.2 - now I really mean it!

--- App Store Releases/v1.0.2/miataru 27.10.13 16.01.xcarchive/Products/Applications/Miataru.app/GHNeighbors-741E79F1E8C49D56.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/bietiekay/code/miataru-ios-app/Libraries/LyoKatoGeoHash/GeoHash/GeoHash/GHNeighbors.m \
-  /Users/bietiekay/code/miataru-ios-app/Libraries/LyoKatoGeoHash/GeoHash/GeoHash/GHNeighbors.h
--- App Store Releases/v1.0.2/miataru 27.10.13 16.01.xcarchive/Products/Applications/Miataru.app/GHRange-652138464BB4BD58.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/bietiekay/code/miataru-ios-app/Libraries/LyoKatoGeoHash/GeoHash/GeoHash/GHRange.m \
-  /Users/bietiekay/code/miataru-ios-app/Libraries/LyoKatoGeoHash/GeoHash/GeoHash/GHRange.h
