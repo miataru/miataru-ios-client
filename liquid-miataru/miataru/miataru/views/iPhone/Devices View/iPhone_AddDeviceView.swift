cb6195b9815b811a4dc681a987585a132855c82e
bietiekay <btk@schrankmonster.de>
2025-06-21 15:21:14 +0200

    rewrite: adding devices now allows to specify color directly

--- liquid-miataru/miataru/miataru/views/iPhone/Devices View/iPhone_AddDeviceView.swift
+++ liquid-miataru/miataru/miataru/views/iPhone/Devices View/iPhone_AddDeviceView.swift
@@ -5,6 +5,7 @@ struct iPhone_AddDeviceView: View {
     @Binding var isPresented: Bool
     @State private var deviceName: String = ""
     @State private var deviceID: String = ""
+    @State private var deviceColor: Color = .gray
     
     var body: some View {
         NavigationView {
@@ -14,6 +15,9 @@ struct iPhone_AddDeviceView: View {
                 }
                 Section(header: Text("device_id")) {
                     TextField("device_id2", text: $deviceID)
+                }
+                Section(header: Text("device_color")) {
+                    ColorPicker("device_color2", selection: $deviceColor)
                 }
             }
             .navigationTitle("new_device")
@@ -25,7 +29,8 @@ struct iPhone_AddDeviceView: View {
                 }
                 ToolbarItem(placement: .confirmationAction) {
                     Button("add") {
-                        let newDevice = KnownDevice(name: deviceName, deviceID: deviceID)
+                        let uiColor = UIColor(deviceColor)
+                        let newDevice = KnownDevice(name: deviceName, deviceID: deviceID, color: uiColor)
                         store.add(device: newDevice)
                         isPresented = false
                     }
