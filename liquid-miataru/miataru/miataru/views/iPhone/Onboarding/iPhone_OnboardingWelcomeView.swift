68fc89650f1335fd50b2336e5aa93bd8d1d33749
Daniel Kirstenpfad <bietiekay@schrankmonster.de>
2025-07-06 18:29:37 +0200

    rewrite: onboarding wizard for different platforms

--- liquid-miataru/miataru/miataru/views/iPhone/Onboarding/iPhone_OnboardingWelcomeView.swift
+++ liquid-miataru/miataru/miataru/views/iPhone/Onboarding/iPhone_OnboardingWelcomeView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct iPhone_OnboardingWelcomeView: View {
     var body: some View {
         VStack {
+            MiataruIcon()
             Text("Welcome to Miataru!")
             Text("This app helps you ...")
         }
