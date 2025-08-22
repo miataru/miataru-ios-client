19ba2d9551a30d258f24dcdda6332a8785894e67
Daniel Kirstenpfad <bietiekay@gmail.com>
2025-08-22 21:20:16 +0200

    Clear last opened device when app backgrounded without map view

--- miataru/miataru/views/iPhone/Devices Views/iPhone_DeviceMapView.swift
+++ miataru/miataru/views/iPhone/Devices Views/iPhone_DeviceMapView.swift
@@ -194,6 +194,7 @@ struct iPhone_DeviceMapView: View {
         }
         .adaptiveToolbarBackground()
         .onAppear {
+            settings.lastOpenedDeviceID = deviceID
             // Use preview parameters if set (for SwiftUI preview)
             if let previewLoc = previewDeviceLocation {
                 deviceLocation = previewLoc
@@ -235,6 +236,9 @@ struct iPhone_DeviceMapView: View {
         }
         .onDisappear {
             stopAutoUpdate()
+            if settings.lastOpenedDeviceID == deviceID {
+                settings.lastOpenedDeviceID = nil
+            }
         }
         .onChange(of: settings.mapUpdateInterval) {
             restartAutoUpdate()
