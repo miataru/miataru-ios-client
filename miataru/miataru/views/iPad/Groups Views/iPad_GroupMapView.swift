de7d8a54bb9e31dd6761098af11b62ff55a463e5
Daniel Kirstenpfad <bietiekay@miataru.com>
2025-12-23 13:35:22 +0100

    Add timeline overlay for device history map

--- miataru/miataru/views/iPad/Groups Views/iPad_GroupMapView.swift
+++ miataru/miataru/views/iPad/Groups Views/iPad_GroupMapView.swift
@@ -401,7 +401,7 @@ struct iPad_GroupMapView: View {
         }
         .navigationDestination(isPresented: $showHistoryView) {
             if let id = historyDeviceID, let device = deviceStore.devices.first(where: { $0.DeviceID == id }) {
-                DeviceHistoryMapView(device: device)
+                iPhone_DeviceHistoryMapView(device: device)
             }
         }
     }
